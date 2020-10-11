445048beafd2280803231647579f23033c331a41
toolsmax0 <xingshijichina@outlook.com>
2020-10-11 22:47:43 +0800

    Added IntelliJ project

---
+++ src/hk/edu/polyu/comp/comp2021/group_project/Main.java
@@ -0,0 +1,9 @@
+package hk.edu.polyu.comp.comp2021.group_project;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello World!");
+    }
+}
