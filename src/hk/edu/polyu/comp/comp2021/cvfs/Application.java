7a417a421a4ecb200acdfcb4bc2e3f81715329ff
Moenupa WANG <mineworldy@outlook.com>
2020-10-18 16:51:43 +0800

    📥 import the SAMPLE from 2021

---
+++ src/hk/edu/polyu/comp/comp2021/cvfs/Application.java
@@ -0,0 +1,17 @@
+/*
+* from Group Project sample
+*
+* Last Modified: NONE
+ */
+
+
+package hk.edu.polyu.comp.comp2021.cvfs;
+
+import hk.edu.polyu.comp.comp2021.cvfs.model.CVFS;
+
+public class Application {
+    public static void main(String[] args) {
+        CVFS cvfs = new CVFS();
+        // initialize and utilize the system
+    }
+}
