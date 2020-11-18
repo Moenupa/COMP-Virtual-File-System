898ecb695302c2082166838159f6999d98d91157
toolsmax0 <xingshijichina@outlook.com>
2020-11-18 21:57:10 +0800

    Refactor files

--- src/hk/edu/polyu/comp/comp2021/cvfs/Application.java
+++ src/hk/edu/polyu/comp/comp2021/cvfs/Application.java
@@ -1,17 +1,27 @@
 /*
-* from Group Project sample
-*
-* Last Modified: NONE
+ * from Group Project sample
+ *
+ * Last Modified: NONE
  */
 
 
 package hk.edu.polyu.comp.comp2021.cvfs;
 
 import hk.edu.polyu.comp.comp2021.cvfs.model.CVFS;
+import hk.edu.polyu.comp.comp2021.cvfs.view.CVFSView;
+import hk.edu.polyu.comp.comp2021.cvfs.controller.CVFSController;
 
 public class Application {
     public static void main(String[] args) {
         CVFS cvfs = new CVFS();
+        CVFSView view = new CVFSView();
+        CVFSController control = new CVFSController(cvfs, view);
         // initialize and utilize the system
+
+        while (true){
+            control.terminal();
+
+            view.updateDir(control.getCur());
+        }
     }
 }
