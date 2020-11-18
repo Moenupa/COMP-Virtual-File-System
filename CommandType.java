56379f1ff44d1f235102e95c0c1805a9bb2dcc29
StrFreeman <62783034+StrFreeman@users.noreply.github.com>
2020-11-18 21:49:59 +0800

    Add files via upload

---
+++ CommandType.java
@@ -0,0 +1,5 @@
+package hk.edu.polyu.comp.comp2021.cvfs.model;
+
+public enum  CommandType {
+    invalid, newDisk, newDoc, newDir, delete, rename, changeDir, list, rList, newSimpleCri, IsDocument, newNegation, newBinaryCri, printAllCriteria, search, rsearch,undo,redo;
+}
