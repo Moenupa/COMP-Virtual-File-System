833670d0acdd060aa5aff8570208653348efa794
StrFreeman <62783034+StrFreeman@users.noreply.github.com>
2020-11-22 14:57:38 +0800

    change error message into exception

--- src/hk/edu/polyu/comp/comp2021/cvfs/controller/CVFSController.java
+++ src/hk/edu/polyu/comp/comp2021/cvfs/controller/CVFSController.java
@@ -55,219 +55,179 @@ public class CVFSController {
         switch (type){
             case newDisk:
                 if(elements.length>2){
-                    System.out.println("command input too long");
-                    return;
+                    throw new IllegalArgumentException("command input too long");
                 }
                 if (elements.length<2){
-                    System.out.println("size of new disk not found");
-                    return;
+                    throw new IllegalArgumentException("size of new disk not found");
                 }
                 if (!elements[1].matches("\\d+")){
-                    System.out.println("invalid size of new disk: "+elements[1]);
-                    return;
+                    throw new IllegalArgumentException("invalid size of new disk: "+elements[1]);
                 }
 
                 cvfs.newDisk(Integer.parseInt(elements[0]));
 
             case newDoc:
                 if (elements.length<4){
-                    System.out.println("name, type or content of new document not found");
-                    return;
+                    throw new IllegalArgumentException("name, type or content of new document not found");
                 }
                 if (!Unit.isValidName(elements[1])){
-                    System.out.println("invalid document name: "+elements[1]);
-                    return;
+                    throw new IllegalArgumentException("invalid document name: "+elements[1]);
                 }
                 if(!DocType.isValidDocType(elements[2])){
-                    System.out.println("invalid document type: "+elements[2]);
-                    return;
+                    throw new IllegalArgumentException("invalid document type: "+elements[2]);
                 }
 
                 cvfs.getCwd().newDoc(elements[1],DocType.parse(elements[2]),elements[3]);
 
             case newDir:
                 if(elements.length>2){
-                    System.out.println("command input too long");
-                    return;
+                    throw new IllegalArgumentException("command input too long");
                 }
                 if (elements.length<2){
-                    System.out.println("name of new directory not found");
-                    return;
+                    throw new IllegalArgumentException("name of new directory not found");
                 }
                 if (!Unit.isValidName(elements[1])){
-                    System.out.println("invalid directory name: "+elements[1]);
-                    return;
+                    throw new IllegalArgumentException("invalid directory name: "+elements[1]);
                 }
 
                 cvfs.getCwd().newDir(elements[1]);
 
             case list:
                 if(elements.length>1){
-                    System.out.println("command input too long");
-                    return;
+                    throw new IllegalArgumentException("command input too long");
                 }
 
                 cvfs.getCwd().list();
 
             case rList:
                 if(elements.length>1){
-                    System.out.println("command input too long");
-                    return;
+                    throw new IllegalArgumentException("command input too long");
                 }
 
                 cvfs.getCwd().down_rList();
 
             case search:
                 if(elements.length>2){
-                    System.out.println("command input too long");
-                    return;
+                    throw new IllegalArgumentException("command input too long");
                 }
                 if (elements.length<2){
-                    System.out.println("name of file to be deleted not found");
-                    return;
+                    throw new IllegalArgumentException("name of file to be deleted not found");
                 }
                 if (!Unit.isValidName(elements[1])){
-                    System.out.println("invalid file name: "+elements[1]);
-                    return;
+                    throw new IllegalArgumentException("invalid file name: "+elements[1]);
                 }
 
                 cvfs.getCwd().search(cvfs.getCri(elements[1]));
 
             case rsearch:
                 if(elements.length>2){
-                    System.out.println("command input too long");
-                    return;
+                    throw new IllegalArgumentException("command input too long");
                 }
                 if (elements.length<2){
-                    System.out.println("name of file to be deleted not found");
-                    return;
+                    throw new IllegalArgumentException("name of file to be deleted not found");
                 }
                 if (!Unit.isValidName(elements[1])){
-                    System.out.println("invalid file name: "+elements[1]);
-                    return;
+                    throw new IllegalArgumentException("invalid file name: "+elements[1]);
                 }
 
                 cvfs.getCwd().rSearch(cvfs.getCri(elements[1]));
 
             case rename:
                 if(elements.length>3){
-                    System.out.println("command inout too long");
-                    return;
+                    throw new IllegalArgumentException("command inout too long");
                 }
                 if (elements.length<3){
-                    System.out.println("old name or new name not found");
-                    return;
+                    throw new IllegalArgumentException("old name or new name not found");
                 }
                 if (!Unit.isValidName(elements[1])){
-                    System.out.println("invalid old name: "+elements[1]);
-                    return;
+                    throw new IllegalArgumentException("invalid old name: "+elements[1]);
                 }
                 if (!Unit.isValidName(elements[2])){
-                    System.out.println("invalid new name: "+elements[2]);
-                    return;
+                    throw new IllegalArgumentException("invalid new name: "+elements[2]);
                 }
 
                 cvfs.getCwd().rename(elements[1],elements[2]);
 
             case delete:
                 if(elements.length>2){
-                    System.out.println("command input too long");
-                    return;
+                    throw new IllegalArgumentException("command input too long");
                 }
                 if (elements.length<2){
-                    System.out.println("name of file to be deleted not found");
-                    return;
+                    throw new IllegalArgumentException("name of file to be deleted not found");
                 }
                 if (!Unit.isValidName(elements[1])){
-                    System.out.println("invalid file name: "+elements[1]);
-                    return;
+                    throw new IllegalArgumentException("invalid file name: "+elements[1]);
                 }
 
                 cvfs.getCwd().delete(elements[1]);
 
             case changeDir:
                 if(elements.length>2){
-                    System.out.println("command input too long");
-                    return;
+                    throw new IllegalArgumentException("command input too long");
                 }
                 if (elements.length<2){
-                    System.out.println("directory name not found");
-                    return;
+                    throw new IllegalArgumentException("directory name not found");
                 }
                 if (!Unit.isValidName(elements[1])){
-                    System.out.println("invalid directory name: "+elements[1]);
-                    return;
+                    throw new IllegalArgumentException("invalid directory name: "+elements[1]);
                 }
 
                 cvfs.changeDir(elements[1]);
 
             case newNegation:
                 if(elements.length>3){
-                    System.out.println("command input too long");
-                    return;
+                    throw new IllegalArgumentException("command input too long");
                 }
                 if (elements.length<3){
-                    System.out.println("number of criterion name is less than 2");
-                    return;
+                    throw new IllegalArgumentException("number of criterion name is less than 2");
                 }
                 if (!Criterion.isValidCriName(elements[1])){
-                    System.out.println("invalid criterion name: "+elements[1]);
-                    return;
+                    throw new IllegalArgumentException("invalid criterion name: "+elements[1]);
                 }
                 if (!Criterion.isValidCriName(elements[2])){
-                    System.out.println("invalid criterion name: "+elements[2]);
-                    return;
+                    throw new IllegalArgumentException("invalid criterion name: "+elements[2]);
                 }
 
                 cvfs.newNegation(elements[1],elements[2]);
 
             case newBinaryCri:
                 if(elements.length>5){
-                    System.out.println("command input too long");
-                    return;
+                    throw new IllegalArgumentException("command input too long");
                 }
                 if (elements.length<5){
-                    System.out.println("number of criterion name is less than 3, or operation not found");
-                    return;
+                    throw new IllegalArgumentException("number of criterion name is less than 3, or operation not found");
                 }
                 if (!Criterion.isValidCriName(elements[1])){
-                    System.out.println("invalid criterion name: "+elements[1]);
-                    return;
+                    throw new IllegalArgumentException("invalid criterion name: "+elements[1]);
                 }
                 if (!Criterion.isValidCriName(elements[2])){
-                    System.out.println("invalid criterion name: "+elements[2]);
-                    return;
+                    throw new IllegalArgumentException("invalid criterion name: "+elements[2]);
                 }
                 if (!Criterion.isValidCriName(elements[4])){
-                    System.out.println("invalid criterion name: "+elements[4]);
-                    return;
+                    throw new IllegalArgumentException("invalid criterion name: "+elements[4]);
                 }
                 if(!(elements[3].equals("&&")||elements[3].equals("||"))){
-                    System.out.println("the logic operation must be && or ||");
-                    return;
+                    throw new IllegalArgumentException("the logic operation must be && or ||");
                 }
 
                 cvfs.newBinaryCri(elements[1],elements[2],elements[3],elements[4]);
 
             case newSimpleCri:
                 if(elements.length>5){
-                    System.out.println("command input too long");
-                    return;
+                    throw new IllegalArgumentException("command input too long");
                 }
                 if (elements.length<5){
-                    System.out.println("criterion name, attribute name, operation or value not found");
-                    return;
+                    throw new IllegalArgumentException("criterion name, attribute name, operation or value not found");
                 }
                 if (!Criterion.isValidCriName(elements[1])){
-                    System.out.println("invalid critertion name: "+elements[1]);
-                    return;
+                    throw new IllegalArgumentException("invalid critertion name: "+elements[1]);
                 }
 
                 cvfs.newSimpleCri(elements[1],elements[2],elements[3],elements[4]);
 
             case printAllCriteria:
                 if(elements.length>1){
-                    System.out.println("command input too long");
+                    throw new IllegalArgumentException("command input too long");
                 }
 
                 cvfs.printAllCriteria();
@@ -275,14 +235,12 @@ public class CVFSController {
 
             case undo:// Those 4 can be saved for later
                 if(elements.length>1){
-                    System.out.println("command input too long");
-                    return;
+                    throw new IllegalArgumentException("command input too long");
                 }
 
             case redo:
                 if(elements.length>1){
-                    System.out.println("command input too long");
-                    return;
+                    throw new IllegalArgumentException("command input too long");
                 }
 
             case load:
