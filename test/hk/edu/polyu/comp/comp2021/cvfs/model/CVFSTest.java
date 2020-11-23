51221189a5875a2fdcfd925c678e793ccf1c6c42
Moenupa WANG <mineworldy@outlook.com>
2020-11-23 13:38:42 +0800

    cvfs fix; test added

--- test/hk/edu/polyu/comp/comp2021/cvfs/model/CVFSTest.java
+++ test/hk/edu/polyu/comp/comp2021/cvfs/model/CVFSTest.java
@@ -3,33 +3,37 @@ package hk.edu.polyu.comp.comp2021.cvfs.model;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
 
 public class CVFSTest {
 
-    CVFS cvfs;
+    public CVFS cvfs;
 
     @Before
     public void setUp() {
         cvfs = new CVFS();
-        cvfs.newSimpleCri("aa", "type", "equals", "\"txt\"");
-        assertTrue(true);
     }
 
     @Test
     public void newDisk() {
+        cvfs.newDisk(1000);
     }
 
     @Test
     public void newSimpleCri() {
+        cvfs.newSimpleCri("aa", "type", "equals", "\"txt\"");
+        cvfs.newSimpleCri("bb", "type", "equals", "\"css\"");
+        assertTrue(true);
     }
 
     @Test
     public void newNegation() {
+        cvfs.newNegation("cc", "aa");
     }
 
     @Test
     public void newBinaryCri() {
+        cvfs.newBinaryCri("dd", "bb", "&&", "cc");
     }
 
     @Test
