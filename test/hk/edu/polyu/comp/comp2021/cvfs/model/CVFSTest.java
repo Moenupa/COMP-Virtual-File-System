9ae77781ae94ec39452d4bda5cb2c576a99c52ed
Moenupa WANG <mineworldy@outlook.com>
2020-11-24 23:40:58 +0800

    Update CVFSTest.java
    test coverage now 90%

--- test/hk/edu/polyu/comp/comp2021/cvfs/model/CVFSTest.java
+++ test/hk/edu/polyu/comp/comp2021/cvfs/model/CVFSTest.java
@@ -25,8 +25,13 @@ public class CVFSTest {
         // below tests have bug and I dont know why
         cvfs.newDisk(100);
         cvfs.getCwd().newDoc("doc1", DocType.TXT, "sample text for doc1");
+        cvfs.load("nowStored"); // error
+        cvfs.load("InvalidNameTestHere"); // error
+        cvfs.store("InvalidNameTestHere"); // error
         cvfs.store("nowStored");
+        cvfs.store("nowStored"); // error
         cvfs.load("nowStored");
+        cvfs.delDisk("nowStored");
     }
 
     @Test
@@ -39,12 +44,42 @@ public class CVFSTest {
 
     @Test
     public void parsePathTest() {
+        String[] correctPath = {"Downloads", ""};
+        cvfs.newDisk(1000);
+        cvfs.getCwd().newDir("Downloads");
+        cvfs.changeDir("Downloads");
+        cvfs.getCwd().newDir("Download1");
+        cvfs.changeDir("..");
 
+        for (int i = 0; i <= 1; i++)
+            System.out.println(cvfs.parsePath("Downloads:Download1")[i]);
     }
 
     @Test
     public void newCriTestPack() {
         cvfs.newSimpleCri("aa", "type", "equals", "\"txt\"");
+        boolean thrown = false;
+        try {
+            cvfs.newSimpleCri("aa", "type", "equals", "\"txt\"");
+        } catch (Exception e) {
+            thrown = true;
+        }
+        assertTrue(thrown);
+        thrown = false;
+        try {
+            cvfs.newNegation("aa", "aa");
+        } catch (Exception e) {
+            thrown = true;
+        }
+        assertTrue(thrown);
+        thrown = false;
+        try {
+            cvfs.newBinaryCri("aa", "aa", "&&", "aa");
+        } catch (Exception e) {
+            thrown = true;
+        }
+        assertTrue(thrown);
+
         cvfs.newSimpleCri("bb", "type", "equals", "\"css\"");
         cvfs.newNegation("cc", "aa");
         cvfs.newBinaryCri("dd", "bb", "&&", "cc");
